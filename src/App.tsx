0132ab264d906f68a1581ebb56e835adf8909e12
Kibatensai <lenutd@yandex.ru>
2021-04-25 20:18:02 +0300

    uploading the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { CurrencyEContainer } from './components/CurrencyEContainer'
+
+function App() {
+    return (
+        <div className="container">
+            <CurrencyEContainer />
+        </div>
+    )
+}
+
+export default App
